a3259afe29b451f4eb5afcab5e909f8ba92e8e44
LanC <632999601@qq.com>
2017-07-24 18:54:48 +0800

    Create Home.vue

---
+++ Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>内容</h1>
+        <button class="logout" @click="logout">退出</button>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        logout: function () {
+            localStorage.removeItem('username')
+            localStorage.removeItem('password')
+            this.logoutSuccess()
+        },
+        loginSuccess: function () {
+            this.$router.replace('/Login')
+        },
+
+        logoutSuccess: function () {
+            this.$router.replace('/Login')
+        },
+    }
+}
+</script>
