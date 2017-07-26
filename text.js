a4e2247ff1d4e5d5bf6f45b16fe4ea183f31c8c6
LanC <632999601@qq.com>
2017-07-26 17:53:46 +0800

    Update text.js

--- text.js
+++ text.js
@@ -34,7 +34,7 @@ var Auth = {
   },
 }
 
-
+main.js
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
