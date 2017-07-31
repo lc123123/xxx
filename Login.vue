95d1677c9b04110084d68431a99c9be6dfdce7e9
LanC <632999601@qq.com>
2017-07-31 20:12:09 +0800

    Update Login.vue

--- Login.vue
+++ Login.vue
@@ -1,55 +1,61 @@
-<template>
-    <div id="app">
-        <label>用户名</label>
-        <input type="text" name="用户名" v-model="username">
-        <br>
-        <label>密码</label>
-        <input type="password" name="密码" v-model="password">
-        <br>
-        <button class="login" @click="login">登录</button>
-    </div>
-</template>
 
-<script>
+import router from '../router'
+import axios from 'axios'
+import md5 from 'js-md5'
+
 export default {
-    data() {
-        return {
-            username: '',
-            password: '',
-            error: ''
-        }
-    },
-    computed: {
+  login: function (username, password) {
+    if (username === 'admin' && password === 'admin') {
 
-    },
-    methods: {
-        check: function () {
-            if (window.localStorage.getItem('username') === null && window.localStorage.getItem('password') === null) {
-                console.log('true')
-                return true
-            } else {
-                console.log('false')
-                return false
-            }
-        },
-        loginSuccess: function () {
-            this.$router.replace('/home')
-        },
+      var params = new URLSearchParams();
+      params.append('username', username);
+      params.append('password', md5(password))
+      axios.post('http://192.168.1.239/iron_wall/admin/test/login', params, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
+        .then((res) => {
+          console.log(res);
+          console.log(res.data.data);
 
-        logoutSuccess: function () {
-            this.$router.replace('/home')
-        },
-        login: function (username, password) {
-            if (this.username === '123' && this.password === '123') {
-                localStorage.setItem('username', username)
-                localStorage.setItem('password', password)
-                console.log('1')
-                this.loginSuccess()
-            } else {
-                console.log('3')
-                return false
-            }
-        }
+        })
+        .catch( (error)=> {
+          console.log(error);
+        });
+      localStorage.setItem('username', username)
+      localStorage.setItem('password', password)
+      return true
+    } else {
+      return false
     }
+  },
+
+  viewdata: function () {
+
+  },
+
+  logout: function () {
+    localStorage.removeItem('username')
+    localStorage.removeItem('password')
+    return true
+  },
+
+  loginSuccess: function () {
+    return ('Home')
+  },
+
+  logoutSuccess: function () {
+    return ('/')
+  },
+
+  check: function () {
+    if (window.localStorage.getItem('username') === null && window.localStorage.getItem('password') === null) {
+      console.log('true')
+      return true
+    } else {
+      console.log('false')
+      return false
+    }
+  },
 }
-</script>
